e4249171a647153a3b2801b04cc74dba4ce0447a
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-04-02 19:58:00 -0400

    Create possible player.routes, PlayerModels, PlayerSeeds

---
+++ models/playerModel.js
@@ -0,0 +1,38 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+// Create the Player model
+module.exports = (sequelize) => {
+  const Player = sequelize.define('Player', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Team_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    Position: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    BattingAverage: {
+      type: DataTypes.DECIMAL(5, 3),
+      allowNull: false
+    },
+    HomeRuns: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    Hits: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    RBIS: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
