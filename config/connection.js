756ed5cb3038fda451aad82db93991adae7be728
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-04-09 17:21:48 -0400

    Modified Connection.JS to use ClearDB Mysql on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+// Create Sequelize instance using the ClearDB database URL
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+  dialect: 'mysql',
+  logging: false // Optionally, set to false to disable logging
+});
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+// Test the connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
