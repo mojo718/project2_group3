df4a03068503ebcbf6fe50d1adca6621eb19697a
Patrick Sheehan <patrick@Patricks-MacBook-Air.local>
2024-04-15 18:12:09 -0400

    updated profile layout and player model

--- models/Player.js
+++ models/Player.js
@@ -20,19 +20,23 @@ Player.init(
     },
     battingAverage: {
       type: DataTypes.DECIMAL(5, 3),
-      allowNull: false
+      allowNull: false,
+      defaultValue: .000
     },
     homeRuns: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      defaultValue: 0
     },
     hits: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      defaultValue: 0
     },
     rbis: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      defaultValue: 0
     },
     manager_id: {
       type: DataTypes.INTEGER,
